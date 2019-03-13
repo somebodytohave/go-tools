0913dd1fcbf1ed189d8c2cdc49eac2ec1afe8492
mecm <645187582@qq.com>
2019-03-13 16:06:48 +0800

    Update file.go

--- file/file.go
+++ file/file.go
@@ -84,7 +84,7 @@ func MustOpen(fileName, filePath string) (*os.File, error) {
 
 	f, err := Open(src+fileName, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
-		return nil, fmt.Errorf("Fail to OpenFile :%v", err)
+		return nil, fmt.Errorf("fail to OpenFile : %v", err)
 	}
 
 	return f, nil
