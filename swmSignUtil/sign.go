fc4886d4d48f562d2f7776af50b10a9c33daf858
sun-wenming <645187582@qq.com>
2019-07-02 09:59:40 +0800

    更新 sha1 base32签名

---
+++ swmSignUtil/sign.go
@@ -0,0 +1,26 @@
+package swmSignUtil
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"github.com/sun-wenming/go-tools/swmRandomUtil"
+)
+
+////将参数进行字典序排序
+//var tempArray  = []string{ ticket, timeStamp, "aa"}
+//sort.Strings(tempArray)
+////将参数字符串拼接成一个字符串进行sha1加密
+//var sha1String string = ""
+//for _, v := range tempArray {
+//	sha1String += v
+//}
+// SignSha1Str  sha1 签名
+func SignSha1(sha1String string) string {
+	h := sha1.New()
+	h.Write([]byte(sha1String))
+	return hex.EncodeToString(h.Sum([]byte("")))
+}
+
+func SignBase32(len int) string {
+	return swmRandomUtil.GetRandomBase32String(len)
+}
