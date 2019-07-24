50a93654b5ccf6db911af01f52fcd8829cdfad2b
e <645187582@qq.com>
2019-07-24 16:23:10 +0800

    Update jwt_test.go

--- mJwtUtil/jwt_test.go
+++ mJwtUtil/jwt_test.go
@@ -9,19 +9,25 @@ import (
 	"log"
 )
 
+const (
+	priKeyName = "jwt_rsa_private_key.pem"
+	pubKeyName = "jwt_rsa_public_key.pem"
+)
+
 // openssl 私钥 公钥
 //openssl genrsa -out rsa_private_key.pem 2048
 //openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
 func main() {
 
-	customClaims := CustomClaims{Data: []byte("abcde")}
+	customClaims := CustomClaims{Data: []byte("")}
 	// TODO 修改私钥公钥 名称
-	token, err := GenRSA256TokenWithFileName(customClaims, "rsa_private_key.pem", "rsa_public_key.pem")
+
+	token, err := GenToken("abcde")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(token)
-	claims, err := ParseRAS256TokenFileName(token, "rsa_private_key.pem", "rsa_public_key.pem")
+	claims, err := ParseToken(token)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,4 +36,21 @@ func main() {
 	fmt.Println(string(claims.Data))
 	fmt.Println("===========")
 
+}
+
+func GenToken(data string) (string, error) {
+	customClaims := CustomClaims{Data: []byte(data)}
+	token, err := GenRSA256TokenWithFileName(customClaims, priKeyName, pubKeyName)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+func ParseToken(token string) (*CustomClaims, error) {
+	claims, err := ParseRAS256TokenFileName(token, priKeyName, pubKeyName)
+	if err != nil {
+		return nil, err
+	}
+	return claims, nil
 }
